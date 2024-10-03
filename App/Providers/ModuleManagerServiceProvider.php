60168204bd79cf26dee3f12f4d49bcfd18cf49c1
mikitosina1 <mikitosina@mail.ru>
2024-10-03 02:36:43 +0200

    fix: removed useless methods

--- App/Providers/ModuleManagerServiceProvider.php
+++ App/Providers/ModuleManagerServiceProvider.php
@@ -16,8 +16,6 @@ class ModuleManagerServiceProvider extends ServiceProvider
 	 */
 	public function boot(): void
 	{
-		$this->registerCommands();
-		$this->registerCommandSchedules();
 		$this->registerTranslations();
 		$this->registerConfig();
 		$this->registerViews();
@@ -31,25 +29,6 @@ class ModuleManagerServiceProvider extends ServiceProvider
 	public function register(): void
 	{
 		$this->app->register(RouteServiceProvider::class);
-	}
-
-	/**
-	 * Register commands in the format of Command::class
-	 */
-	protected function registerCommands(): void
-	{
-		// $this->commands([]);
-	}
-
-	/**
-	 * Register command Schedules.
-	 */
-	protected function registerCommandSchedules(): void
-	{
-		// $this->app->booted(function () {
-		//     $schedule = $this->app->make(Schedule::class);
-		//     $schedule->command('inspire')->hourly();
-		// });
 	}
 
 	/**
