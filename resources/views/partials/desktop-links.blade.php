c07abc2630a8cd1538a4b1a892af1243448ccc17
mikitosina1 <mikitosina@mail.ru>
2025-05-18 00:01:39 +0200

    feat: manage modules only for admin

--- resources/views/partials/desktop-links.blade.php
+++ resources/views/partials/desktop-links.blade.php
@@ -1,5 +1,11 @@
-<div class="nav-link-div">
-    <x-nav-link :href="route('module.index')" :active="request()->routeIs('module.index')">
-        @lang('modulemanager::module_manager_lang.manage_modules')
-    </x-nav-link>
-</div>
+@php
+	use App\Enums\UserRole;
+	$isAdmin = session('role') === UserRole::Admin->value;
+@endphp
+@if($isAdmin)
+	<div class="nav-link-div">
+		<x-nav-link :href="route('module.index')" :active="request()->routeIs('module.index')">
+			@lang('modulemanager::module_manager_lang.manage_modules')
+		</x-nav-link>
+	</div>
+@endif
