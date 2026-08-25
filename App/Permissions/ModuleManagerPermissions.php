be62da61164f680c547cddab59fc4af259dde934
mikitosina1 <mikitosina27@gmail.com>
2026-08-25 19:10:05 +0200

    fix: help documentation

--- App/Permissions/ModuleManagerPermissions.php
+++ App/Permissions/ModuleManagerPermissions.php
@@ -3,7 +3,11 @@
 namespace Modules\ModuleManager\App\Permissions;
 
 use App\Contracts\ModulePermissions;
+use Modules\ModuleManager\App\Services\ModulePermissionService;
 
+/**
+ * @see ModulePermissionService
+ */
 final class ModuleManagerPermissions implements ModulePermissions
 {
     public static function all(): array
